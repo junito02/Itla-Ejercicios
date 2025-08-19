c9348d08ed8f7c123eeb4b0dec8c66f1403389f8
Junior Alejandro <junioralejdandrotiburcio@gmail.com>
2025-08-19 13:59:58 -0400

    adding-perrio-project

---
+++ js/perritos.js
@@ -0,0 +1,21 @@
+function fetching() {
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then((res) => res.json())
+    .then((data) => {
+      document
+        .getElementById("fetch-perrito")
+        .setAttribute("src", data.message);
+    });
+}
+
+function xmlf() {
+  let xml = new XMLHttpRequest();
+  xml.open("GET", "https://dog.ceo/api/breeds/image/random", true);
+  xml.onreadystatechange = function () {
+    if (xml.readyState == 4 && xml.status == 200) {
+      let data = JSON.parse(xml.responseText);
+      document.getElementById("xml-perrito").setAttribute("src", data.message);
+    }
+  };
+  xml.send()
+}
